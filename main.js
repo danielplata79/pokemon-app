ec1950e32db31106e996a5283541e65ae4ba3de7
unknown <danielplata79@gmail.com>
2021-09-17 16:48:50 -0500

    Full menu working

--- main.js
+++ main.js
@@ -8,28 +8,50 @@ console.log("1. Charmander");
 console.log("2. Ivysaur");
 console.log("3. Bulbasaur");
 console.log(" ");
-console.log("4. Select another pokemon");
+console.log("4. Type a random number and select another pokemon!");
 console.log("5. List all Pokemones availables");
 console.log(" ");
 console.log("Copyright Daniel Plata - ull dominate, for sure.");
 console.log("");
 
 // Menu - asking
-var asking = prompt("Hello, select your pokemon to show more details!");
+var asking = prompt("Hello, select your pokemon to show more details! : ");
 asking = parseInt(asking);
 
 // Menu - logic
-
-const getPoke = () => {
-	var getApi = pokeflow.getName(asking);
-	console.log("esto : " + getApi);
+const getPoke = (asking) => {
+	console.log("");	
+	return console.log("Felicidades! : Tu nuevo Pokemon es: " + pokeflow.getName(asking));	
 };
 
 switch (asking) {
 	// Charmander
-	case 6:
-	case 5:
-	case 4:
-		getPoke();
+	case 1:
+		getPoke(4);
 	break;
+
+	// Ivysaur
+	case 2:
+		getPoke(2);
+	break;
+
+	// Bulbasaur
+	case 3:
+		getPoke(1);
+	break;
+
+	// Select another (list all)
+	case 4:
+		console.log("");
+		const askingRandom =  prompt("Type a random number!: ");
+		console.log("Congratulations! you got: " + pokeflow.getName(askingRandom));		
+	break;			
+
+	case 5:
+		console.log("");
+		console.log("Select one Pokemon : " +  pokeflow.all());
+	break;	
 };
+
+
+// console.log(pokeflow.getId("Bulbasaur"));
