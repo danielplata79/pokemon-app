2e2e0621873ba9f230b9bcf3b76c88437bafaf25
Daniel Plata <danielplata79@gmail.com>
2021-10-17 23:06:07 -0500

    app completed

--- libs.js
+++ libs.js
@@ -9,7 +9,7 @@ module.exports = {
 	showTitle : function () {
 		console.log("");
 		console.log("---------------------------------------------------------------");
-		console.log(figlet.textSync("Pokemón API"));
+		console.log(figlet.textSync("Poke -- APP"));
 		console.log("---------------------------------------------------------------");
 		console.log("");
 	},
@@ -21,11 +21,55 @@ module.exports = {
 	},
 	
 	// Menu - getPoke : get pokemon
-	getPoke : function() {
-		var asking = myAsking;
+	getPoke : function(asking) {
+		var asking = asking; 
 		module.exports.consoleClear();
-		return console.log("Felicidades! : Tu nuevo Pokemon es: " + pokeflow.getName(myAsking));
-	}
-}; // Module Export - END
 
+		const pokeName = pokeflow.getName(asking);
+		const pokeToLower = pokeName.toLowerCase();
+		const pokeReq = console.log("Felicidades! : Tu nuevo Pokemon es: " + pokeflow.getName(asking)); 
+		const pokeImgReq = pokeImg.getSprite(pokeToLower);
 
+		imgConverter(pokeImgReq, {
+			colored: true,
+			size: {
+				height: "30",
+				width: "30"
+			}, 
+		}, (err, converted) => {	
+			console.log(err || converted);            
+		}); 
+	}, 
+
+	threePrincipals : function(pokemon) {
+		const pokeName = pokeflow.getName(pokemon);
+		const pokeToLower= pokeName.toLowerCase();
+		console.log("aqui sif unciona : " + pokeToLower);
+		const pokeSprite = pokeImg.getSprite(pokeToLower); 
+		imgConverter(pokeSprite, {
+			colored: true,
+			size: {
+				height: "30",
+				width: "30"
+			},
+		},(err, converted) => {
+			console.log(err || converted);
+		}); 
+		console.log("Felicidades! : Tu nuevo Pokemon es: " + pokeName);	
+	},
+
+	getPokeImg : function (pokemonName) {
+		const pokeToLower = pokemonName.toLowerCase();
+		const pokeSprite = pokeImg.getSprite(pokeToLower); 
+		
+		imgConverter(pokeSprite, {
+			color: true,
+			size: {
+				height: "30",
+				width: "30"
+			},
+		}, (err ,converted) => {
+			console.log(err || converted);
+		}); 
+	} 
+}; // Module Export - END 
--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-// Requiring Pokemon API
 const pokeflow = require("pokemon");
 const prompt = require("prompt-sync")();
 const figlet = require("figlet");
@@ -11,12 +10,14 @@ var asking;
 // Menu 
 module.exports = {
 	startApp : function () {
+		libs.consoleClear();
 		console.log("1. Charmander");
 		console.log("2. Ivysaur");
 		console.log("3. Bulbasaur");
 		console.log(" ");
 		console.log("4. Type a random number and select another pokemon!");
 		console.log("5. List all Pokemones availables");
+		console.log("6. Type an Pokemon Name and print an IMG");
 		console.log(" ");
 		console.log("---------------------------------------------------------------");
 		console.log("Copyright Daniel Plata");
@@ -24,34 +25,33 @@ module.exports = {
 	
 		// Menu - asking
 		asking = prompt("Hello, select your pokemon to show more details! : ");
-		myAsking = parseInt(asking);
+		myAsking = parseInt(asking); 
 
 		// Menu choise selection
 		switch (myAsking) {
 			// Charmander
 			case 1:
-				libs.consoleClear();
-				console.log("hola");
-				libs.getPoke(4);
+				libs.consoleClear(); 
+				libs.threePrincipals(4);
 			break;
 
 			// Ivysaur
 			case 2:
 				libs.consoleClear();
-				libs.getPoke(2);
+				libs.threePrincipals(2);
 			break;
 
 			// Bulbasaur
 			case 3:
 				libs.consoleClear();
-				libs.getPoke(1);
+				libs.threePrincipals(1);
 			break;
 
 			// Select another (list all)
-			case 4:
-				libs.consoleClear();
-				const askingRandom =  prompt("Type a random number!: ");
-				console.log("Congratulations! you got: " + pokeflow.getName(askingRandom));		
+			case 4: 
+				//libs.consoleClear();
+				const askingRandom =  prompt("Type a random number!: "); 
+				libs.getPoke(askingRandom);
 			break;			
 
 			case 5:
@@ -59,17 +59,16 @@ module.exports = {
 				console.log("");
 				console.log("Select one Pokemon : " +  pokeflow.all());
 			break;	
+
+			case 6:
+				libs.consoleClear();
+				console.log(); 
+				const askingName =  prompt("Type your Pokemon Name!: "); 
+				libs.getPokeImg(askingName);
 		};
 	}
 };
 
 // App start
 libs.showTitle();
-module.exports.startApp();
-
-var squirtleImg = pokeImg.getSprite("squirtle"); 
-console.log(squirtleImg);
-
-imgConverter("./7.png", (err, converted) => {
-	console.log(err || converted);
-}); 
+module.exports.startApp(); 
